02c7ec8d7b3ec96caa8dd502cbe52442e2a9380b
Uduv <ulysseduvillier@gmail.com>
2024-09-30 14:31:36 +0200

    add dashboard

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,11 +1,13 @@
 // packages/frontend/src/App.js
 import React from 'react';
 import Weather from './Weather';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Dashboard />
     </div>
   );
 }
---
+++ packages/frontend/src/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        margin: '60px',
+    }}>
+    <iframe 
+      title="Weather Dashboard"
+      style={{background: '#21313C', border: 'none', borderRadius: '20px', boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)', width: '95vw', height: '100vh'}} 
+      src="https://charts.mongodb.com/charts-project-0-dcvjfon/embed/dashboards?id=66df3492-4888-431d-8f6b-641460a74581&theme=dark&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=true&scalingWidth=fix&scalingHeight=fix">
+
+    </iframe>
+    </div>
+  )
+}
+
+
