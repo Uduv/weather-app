1d538de44913341811d970ecee37e0a865cf9366
Uduv <ulysseduvillier@gmail.com>
2024-09-30 15:34:00 +0200

    edit link of chart

--- packages/frontend/src/Dashboard.js
+++ packages/frontend/src/Dashboard.js
@@ -6,7 +6,7 @@ export default function Dashboard() {
     <iframe 
       title="Weather Dashboard"
       style={{background: '#21313C', border: 'none', borderRadius: '20px', boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)', width: '95vw', height: '100vh'}} 
-      src="https://charts.mongodb.com/charts-project-0-dcvjfon/embed/dashboards?id=66df3492-4888-431d-8f6b-641460a74581&theme=dark&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=true&scalingWidth=fix&scalingHeight=fix">
+      src="https://charts.mongodb.com/charts-project-0-mxqgaks/embed/dashboards?id=66fa9b55-97d9-4edd-80ef-10736a8b7ac7&theme=dark&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=true&scalingWidth=scale&scalingHeight=fixed">
 
     </iframe>
     </div>
